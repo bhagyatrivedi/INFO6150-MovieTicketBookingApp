4439920272433dcbc89353740464fa11f991c8c6
bhagyatrivedi <144855580+bhagyatrivedi@users.noreply.github.com>
2024-04-16 15:30:15 -0400

    Added user specific models, routes and Auth middleware

---
+++ backend/Models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    imagePath: {
+        type: String
+    },
+    type: { 
+        type: String,
+        required: true,
+        enum: ['customer', 'admin'] 
+    },
+    favoriteGenres: {
+        type: [String], 
+        required: false 
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
