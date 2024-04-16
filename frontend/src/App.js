098ed8e42c7869237d9df5cae90586641f04b727
Jayeshc224 <chindarkar.j@northeastern.edu>
2024-04-15 23:22:49 -0400

    Added Booking History feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../src/components/Home/Home'; // Example page component
 import Login from '../src/components/Login/Login'; // Example page component
+import BookingHistory from './components/BookingHistory/BookingHistory';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/login" element={<Login />} />
+            <Route path="/bookinghistory" element={<BookingHistory />} />
           </Routes>
         </Box>
         <Footer />
